b9870241f56115a805ce0c48b93c2c0528735353
Mikhail Vinogradov <movaldemar@gmail.com>
2023-06-15 21:46:44 +0300

    start

---
+++ models.py
@@ -0,0 +1,43 @@
+import os
+
+from sqlalchemy import JSON, Column, Integer, String
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+PG_USER = os.getenv("PG_USER", "postgres")
+PG_PASSWORD = os.getenv("PG_PASSWORD", "openBD")
+PG_DB = os.getenv("PG_DB", "netology_async_hw")
+PG_HOST = os.getenv("PG_HOST", "127.0.0.1")
+PG_PORT = os.getenv("PG_PORT", "5432")
+
+PG_DSN = f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+engine = create_async_engine(PG_DSN)
+Base = declarative_base()
+Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class SwapiPeople(Base):
+    __tablename__ = "swapi_people"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    height = Column(String)
+    mass = Column(String)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    eye_color = Column(String)
+    birth_year = Column(String)
+    gender = Column(String)
+    homeworld = Column(String)
+    films = Column(String)
+    species = Column(String)
+    vehicles = Column(String)
+    starships = Column(String)
+    json = Column(JSON)
+
+
+# # films - строка с названиями фильмов через запятую - SSS вытащить из jsona -title
+# # homeworld - SSS вытащить из jsona - name
+# # species - строка с названиями типов через запятую - SSS  jsona - name (droid)
+# # starships - строка с названиями кораблей через запятую -SSS jsona - name
+# # vehicles - строка с названиями транспорта через запятую -SSS jsona - name
---
+++ swapi_async.py
@@ -0,0 +1,82 @@
+import asyncio
+import datetime
+import aiohttp
+from more_itertools import chunked
+
+from models import Base, Session, SwapiPeople, engine
+
+MAX_CHUNK_SIZE = 5
+
+async def get_json(url_x):
+    session = aiohttp.ClientSession()
+    response = await session.get(url_x)
+    json_data = await response.json()
+    await session.close()
+    if "detail" in json_data:
+        print(f"ой!>>>>>>>>>>>>>>>>>>>>>> похоже, база пустая - {json_data}")
+    else:
+        if "name" in json_data:
+            print(json_data["name"])
+    return json_data
+
+
+async def get_in(json_in, name_in):
+    r = []
+    for x_in in json_in:
+        json_x = await get_json(x_in)
+        r.append(json_x[name_in])
+    rr = ", ".join(r) if r else "None"
+    return rr
+
+
+async def insert_to_db(people_json_list):
+    async with Session() as session:
+        swapi_people_list = [
+            SwapiPeople(
+                json=json_data,
+                name=json_data["name"],
+                height=json_data["height"],
+                mass=json_data["mass"],
+                hair_color=json_data["hair_color"],
+                skin_color=json_data["skin_color"],
+                eye_color=json_data["eye_color"],
+                birth_year=json_data["birth_year"],
+                gender=json_data["gender"],
+                homeworld=await get_in([json_data["homeworld"]], "name"),
+                films=await get_in(json_data["films"], "title"),
+                species=await get_in(json_data["species"], "name"),
+                vehicles=await get_in(json_data["vehicles"], "name"),
+                starships=await get_in(json_data["starships"], "name"),
+            )
+            for json_data in people_json_list
+            if "name" in json_data
+        ]
+        session.add_all(swapi_people_list)
+        await session.commit()
+
+
+async def main():
+    async with engine.begin() as con:
+        await con.run_sync(Base.metadata.create_all)
+    await engine.dispose()
+
+    for ids_chunk in chunked(range(1, 91), MAX_CHUNK_SIZE):
+        print(ids_chunk)
+        get_people_coros = [
+            get_json(f"https://swapi.dev/api/people/{people_id}")
+            for people_id in ids_chunk
+        ]
+        people_json_list = await asyncio.gather(*get_people_coros)
+        asyncio.create_task(insert_to_db(people_json_list))
+
+    current_task = asyncio.current_task()
+    tasks_sets = asyncio.all_tasks()
+    tasks_sets.remove(current_task)
+
+    await asyncio.gather(*tasks_sets)
+
+
+start = datetime.datetime.now()
+asyncio.run(main())
+print("work!")
+print(datetime.datetime.now() - start)
